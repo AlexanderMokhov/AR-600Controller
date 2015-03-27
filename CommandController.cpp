0a4173b33b92d6e7c13c051b1b317f7f4eb38514
Pavel <tarasradio@mail.ru>
2015-03-27 17:19:44 +0300

    Update Command Controller
    Add Update and other...

--- CommandController.cpp
+++ CommandController.cpp
@@ -10,3 +10,17 @@ CommandController::~CommandController()
 
 }
 
+CommandController::Update(uint mTime, MBWrite &buffer)
+{
+    if(mCommandsList.at(Id).mTime==mTime)//совпало время
+    {
+        //читаем все команды с таким временем
+        while(mCommandsList.at(Id).mTime==mTime)
+        {
+            //записываем значение в мотор и проверяем следующую команду
+            buffer.MOTOR_ANGLE_set(mCommandsList.at(Id).mChanelNomber,mCommandsList.at(Id).mPosition);
+            Id++;
+        }
+    }
+}
+
--- CommandController.h
+++ CommandController.h
@@ -1,14 +1,23 @@
 #ifndef COMMANDCONTROLLER_H
 #define COMMANDCONTROLLER_H
 
+#include "QFile"
+#include "QIODevice"
+#include "QList"
+#include "DriverCommand.h"
+#include "MBWrite.h"
 
 // управление конмандами,
 //содержит список команд и по заданному времени обновляет буфер
 class CommandController
 {
+private:
+    QList<DriverCommand> mCommandsList;//список команд
+    uint Id;
 public:
        CommandController();
     ~CommandController();
+       Update(uint mTime, MBWrite &buffer);
 
 //       Update (время, буфер) искать в списке команд время и по найденому значению заполнять буфер
 //       загрузка списка команд из файла, если файл не праильного формата, сообщать об этом, список команд обнулять
